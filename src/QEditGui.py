660df0c74875aad2e06053208b9c02229c09e918
Daphne Schössow <daphne.schoessow@ikt.uni-hannover.de>
2022-02-21 16:04:39 +0100

    minor bugfixes, updated Launcher.exe

--- src/QEditGui.py
+++ src/QEditGui.py
@@ -488,26 +488,26 @@ class EditGui(QWidget):
                 else:
                     file = self.parent().structure.filename
             self.preview_gui = StackedWindowGui(file, popup=False, preview=True)
+            if self.preview_gui.layout() is not None:
+                if "stylesheet" in self.parent().structure.keys():
+                    with open(self.parent().structure["stylesheet"], 'r') as css_file:
+                        css_data = css_file.read().replace('\n', '')
+                else:
+                    with open("stylesheets/minimal.qss", 'r') as css_file:
+                        css_data = css_file.read().replace('\n', '')
+                p_widget = QWidget()
+                p_widget.setLayout(self.preview_gui.layout())
+                p_widget.setStyleSheet(css_data)
+                self.prev_widget.addWidget(p_widget)
 
-            if "stylesheet" in self.parent().structure.keys():
-                with open(self.parent().structure["stylesheet"], 'r') as css_file:
-                    css_data = css_file.read().replace('\n', '')
-            else:
-                with open("stylesheets/minimal.qss", 'r') as css_file:
-                    css_data = css_file.read().replace('\n', '')
-            p_widget = QWidget()
-            p_widget.setLayout(self.preview_gui.layout())
-            p_widget.setStyleSheet(css_data)
-            self.prev_widget.addWidget(p_widget)
-
-            if self.current_item is not None and self.current_item.parent() is None:
-                pass
-            elif self.current_item is not None and self.current_item.parent().parent() is None:
-                self.preview_gui.Stack.setCurrentIndex(
-                    self.parent().structure.sections.index(self.current_item.text(0)))
-            elif self.current_item is not None and self.current_item.parent().parent().parent() is None:
-                self.preview_gui.Stack.setCurrentIndex(
-                    self.parent().structure.sections.index(self.current_item.parent().text(0)))
+                if self.current_item is not None and self.current_item.parent() is None:
+                    pass
+                elif self.current_item is not None and self.current_item.parent().parent() is None:
+                    self.preview_gui.Stack.setCurrentIndex(
+                        self.parent().structure.sections.index(self.current_item.text(0)))
+                elif self.current_item is not None and self.current_item.parent().parent().parent() is None:
+                    self.preview_gui.Stack.setCurrentIndex(
+                        self.parent().structure.sections.index(self.current_item.parent().text(0)))
 
             if file == "./tmp.txt":
                 os.remove(file)
@@ -597,7 +597,6 @@ class EditGui(QWidget):
                             val_field = TextEdit(self.parent().structure[field] if field in self.parent().structure.keys() else "")
                             val_field.editingFinished.connect(self.edit_done)
                         elif field == "save_after":
-                            print("display")
                             val_field = QComboBox()
                             val_field.setObjectName("save_after")
                             current_pages = self.parent().structure.sections
@@ -606,6 +605,7 @@ class EditGui(QWidget):
                                 val_field.addItems(current_pages)
                                 selected = current_pages.index(self.parent().structure[field]) if (field in self.parent().structure.keys() and self.parent().structure[field] in current_pages) else -1
                                 val_field.setCurrentIndex(selected if selected > -1 else len(current_pages)-1)
+                                self.parent().structure[field] = val_field.currentText()
                         elif field == "stylesheet":
                             val_field = QHBoxLayout()
                             if field in self.parent().structure.keys():
@@ -660,7 +660,7 @@ class EditGui(QWidget):
                         if val_field is not None:
                             self.edit_layout.addRow(QLabel(k), val_field)
                             val_field.setToolTip(tooltips[k])
-                if self.preview_gui is not None:
+                if self.preview_gui is not None and self.preview_gui.layout() is not None:
                     self.preview_gui.Stack.setCurrentIndex(self.parent().structure.sections.index(val.data()))
             else:  # question
                 question_data = self.parent().structure[val.parent().data()][val.data()]
@@ -899,7 +899,6 @@ class EditGui(QWidget):
                         self.rand_filechooser.setEnabled(False)
                 elif lbl == "save_after":
                     new_val = self.sender().currentText()
-                    print("active")
                 else:
                     lbl = lbl.lower()
                     new_val = self.sender().currentText()
@@ -981,14 +980,10 @@ class EditGui(QWidget):
         """
         while layout.count():
             child = layout.takeAt(0)
-            if child is not None and child.widget() and child.widget().objectName() == "save_after":
-                print("should be removed")
             if child.widget() and (not child.widget() == self.questiontype) and \
                     (not child.widget() == self.qss_filechooser) and (not child.widget() == self.qss_filename)\
                     and (not child.widget() == self.pw_file) and \
                     (not child.widget() == self.rand_file) and (not child.widget() == self.rand_filechooser):
-                if child.widget().objectName() == "save_after":
-                    print("...reemove...")
                 child.widget().deleteLater()
             elif child.widget() == self.questiontype:
                 self.questiontype.hide()
@@ -1161,7 +1156,6 @@ class EditGui(QWidget):
                 self.parent().structure[text] = new_page
                 if "save_after" not in self.parent().structure.keys() or self.parent().structure["save_after"] is None:
                     self.parent().structure["save_after"] = text
-                    print("update")
             else:
                 page = QTreeWidgetItem(self.treeview.itemAt(0, 0), self.treeview.currentItem())
                 page.setText(0, text)
@@ -1243,6 +1237,8 @@ class EditGui(QWidget):
         self.parent().redo_stack.clear()
         self.parent().redoaction.setEnabled(False)
         self.parent().structure.pop(item.text(0))
+        if "save_after" in self.parent().structure.keys() and self.parent().structure["save_after"] == item.text(0):
+            self.parent().structure["save_after"] = None
         sip.delete(item)  # delete AFTER last ref!
         self.treeview.clearSelection()
         self.current_item = None
